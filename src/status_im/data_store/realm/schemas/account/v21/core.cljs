7133934e575da0976571f57a5161ca5a582e2b83
alwxndr <alwxndr@gmail.com>
2018-01-29 23:07:11 +0100

    update for migration

--- src/status_im/data_store/realm/schemas/account/v21/core.cljs
+++ src/status_im/data_store/realm/schemas/account/v21/core.cljs
@@ -3,7 +3,7 @@
             [status-im.data-store.realm.schemas.account.v1.chat-contact :as chat-contact]
             [status-im.data-store.realm.schemas.account.v19.contact :as contact]
             [status-im.data-store.realm.schemas.account.v20.discover :as discover]
-            [status-im.data-store.realm.schemas.account.v21.message :as message]
+            [status-im.data-store.realm.schemas.account.v19.message :as message]
             [status-im.data-store.realm.schemas.account.v12.pending-message :as pending-message]
             [status-im.data-store.realm.schemas.account.v1.processed-message :as processed-message]
             [status-im.data-store.realm.schemas.account.v19.request :as request]
@@ -12,7 +12,8 @@
             [status-im.data-store.realm.schemas.account.v5.group-contact :as group-contact]
             [status-im.data-store.realm.schemas.account.v8.local-storage :as local-storage]
             [taoensso.timbre :as log]
-            [cljs.reader :as reader]))
+            [cljs.reader :as reader]
+            [clojure.string :as str]))
 
 (def schema [chat/schema
              chat-contact/schema
@@ -27,6 +28,16 @@
              group-contact/schema
              local-storage/schema])
 
+(defn remove-location-messages [old-realm new-realm]
+  (let [messages (.objects new-realm "message")]
+    (dotimes [i (.-length messages)]
+      (let [message (aget messages i)
+            content (aget message "content")
+            type    (aget message "content-type")]
+        (when (and (= type "command")
+                   (> (str/index-of content "command=location") -1))
+          (aset message "show?" false))))))
+
 (defn migration [old-realm new-realm]
   (log/debug "migrating v21 account database: " old-realm new-realm)
-  (message/migration old-realm new-realm))
+  (remove-location-messages old-realm new-realm))
